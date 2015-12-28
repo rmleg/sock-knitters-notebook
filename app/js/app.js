6631d33a3c947c257c8bb9022960fed87aa10142
Rachel Leggett <rleggett@umich.edu>
2015-12-28 14:15:16 -0500

    Cuff down flap heel recipe complete

--- app/js/app.js
+++ app/js/app.js
@@ -11,4 +11,36 @@ sknApp.controller('StitchCalc', ['$scope', function($scope) {
         }
         else return 0;
     };
+
+    $scope.saveNeedSts = function(stsPerIn, circumference, mult) {
+        /* find needed stitches without concern for multiples */
+        var need = 0;
+        if(stsPerIn && circumference) {
+            var need = Math.floor(0.9 * stsPerIn * circumference);
+        }
+
+        /* find needed stitches - largest multiple of mult smaller than need */
+        if(mult) { //if mult is not zero, calculate
+            if(need % mult === 0) {
+                return need;
+            }
+            else {
+                for(var i = 1; i < multSize; ++i) {
+                    if((need - i) % mult === 0) {
+                        return need - i;
+                    }
+                }
+            }
+        }
+        return need;
+    };
+
+    $scope.legLength = function(heelType) {
+        if(heelType === "flap") {
+            return "six";
+        }
+        else
+            return "seven";
+    };
+        
 }]);
