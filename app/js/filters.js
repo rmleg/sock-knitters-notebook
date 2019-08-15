c9098815a8891c33c0a7f37500049746881643b6
Rachel Leggett <rleggett@umich.edu>
2019-08-14 23:41:20 -0400

    Add better commenting to floorMult filter.

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,15 +1,20 @@
 /* Filter for finding the next-smallest multiple of input number */
 angular.module('sknFloorMultFilter', []).filter('floorMult', function() {
     return function(input, multSize) {
+        /* if input is already a multiple of multSize, return */
         if(input % multSize === 0) {
             return input;
         }
         else {
+            /* Ensure input is a whole number */
             var inputFloor = Math.floor(input);
+            /* if inputFloor is a multiple of multSize, return inputFloor */
             if(inputFloor % multSize === 0) {
                 return inputFloor;
             }
             else {
+                /* subtract 1 from inputFloor until inputFloor
+                is a mulple of multSize. Return modified inputFloor.*/
                 for(var i = 1; i < multSize; ++i) {
                     if((inputFloor - i) % multSize === 0) {
                         return inputFloor - i;
